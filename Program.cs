6dcd9c58551809fe9ba16a856aa3cb9e97281abc
pelayowindel-dynata <windel.pelayo@dynata.com>
2025-08-02 17:14:26 +0800

    move to separate file

--- Program.cs
+++ Program.cs
@@ -4,30 +4,6 @@ using MeltySynth;
 
 namespace livestage_console;
 
-// Continuous audio provider for MeltySynth
-class SynthWaveProvider : IWaveProvider
-{
-    private readonly Synthesizer synth;
-    private readonly WaveFormat waveFormat;
-
-    public SynthWaveProvider(Synthesizer synth, int sampleRate = 44100)
-    {
-        this.synth = synth;
-        waveFormat = WaveFormat.CreateIeeeFloatWaveFormat(sampleRate, 2);
-    }
-
-    public WaveFormat WaveFormat => waveFormat;
-
-    public int Read(byte[] buffer, int offset, int count)
-    {
-        int samples = count / sizeof(float);
-        var floatBuffer = new float[samples];
-        synth.RenderInterleaved(floatBuffer);
-        Buffer.BlockCopy(floatBuffer, 0, buffer, offset, count);
-        return count;
-    }
-}
-
 class Program
 {
     private static Synthesizer? synth;
